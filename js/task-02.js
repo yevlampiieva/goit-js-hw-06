bcbb03eb4624ea567beb152399070726aecd88bf
Alla Yevlampiieva <modan1679@gmail.com>
2023-04-22 05:47:28 +0300

    Create task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,16 @@
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const ingredientsContainer = [];
+
+const markup = ingredients.map((ingredient) => {
+  const ingredientsItemEl = document.createElement("li");
+  ingredientsItemEl.textContent = `${ingredient}`;
+  ingredientsItemEl.classList.add("item");
+  ingredientsContainer.push(ingredientsItemEl);
+});
+
+ingredientsEl.append(...ingredientsContainer);
+
+// console.log(ingredientsEl);
