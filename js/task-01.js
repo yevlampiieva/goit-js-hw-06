40a58286ada5662b5e8da2c48d55f05f267dca4f
Alla Yevlampiieva <modan1679@gmail.com>
2023-04-22 04:08:05 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categories = document.querySelectorAll(".item");
+
+console.log("Number of categories: ", categories.length);
+
+categories.forEach((category) => {
+  console.log("Category: ", category.firstElementChild.textContent);
+  console.log("Elements: ", category.lastElementChild.querySelectorAll("li").length);
+});
