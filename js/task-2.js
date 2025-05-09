09758ad6c8231c77f6ab29eac9c64846908b1df8
Alla Yevlampiieva <modan1679@gmail.com>
2025-05-10 01:18:10 +0300

    Do task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,36 @@
 "use strict";
+
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = [...items];
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index >= 0) {
+      this.#items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
